de99b33688fab4f3426f51a5fdb7600e59b418c6
Marjorie Delos Santos <124019950+rie016@users.noreply.github.com>
2023-11-21 13:23:40 +0800

    Add files via upload

---
+++ backend/day-01/day01.py
@@ -0,0 +1,14 @@
+def main():
+    # Replace the placeholder values with your information
+    your_name = "Marjorie"
+    name_of_department = "Department of Software and Web Development"
+    about_you = "I am always curious about anything"
+
+    # Generate the output
+    output = f"Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!\nOne thing about me is that {about_you}."
+
+    # Print the output
+    print(output)
+
+if __name__ == "__main__":
+    main()
