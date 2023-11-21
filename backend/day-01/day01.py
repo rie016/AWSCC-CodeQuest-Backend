9f86468a7d9aa2790ec8d162b5253de1c30b3912
Marjorie Delos Santos <124019950+rie016@users.noreply.github.com>
2023-11-21 13:30:14 +0800

    Update day01.py

--- backend/day-01/day01.py
+++ backend/day-01/day01.py
@@ -1,8 +1,8 @@
 def main():
-    # Replace the placeholder values with your information
-    your_name = "Marjorie"
-    name_of_department = "Department of Software and Web Development"
-    about_you = "I am always curious about anything"
+    # Get user input for name, department, and something about themselves
+    your_name = input("Enter your name: ")
+    name_of_department = input("Enter the name of your department: ")
+    about_you = input("Share something about yourself: ")
 
     # Generate the output
     output = f"Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!\nOne thing about me is that {about_you}."
