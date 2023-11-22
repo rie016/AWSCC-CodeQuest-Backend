9b2ece713717b429bee65321f20b2f4fd9885586
Marjorie Delos Santos <124019950+rie016@users.noreply.github.com>
2023-11-22 15:24:47 +0800

    Update test.py

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,6 @@
-name = Marjorie
-age = 19
-isMale = female
-department = DSWD
+name = none
+age = none
+isMale = none
+department = none
 
 print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
